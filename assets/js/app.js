bbcfc966ab678bc47a1a2227891131b93be0f323
Jordan Elver <jordan@elver.me>
2020-04-04 17:20:20 +0100

    Add TurboLinks

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,11 @@ import css from "../css/app.css"
 // Import dependencies
 //
 import "phoenix_html"
+import TurboLinks from "turbolinks"
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+TurboLinks.start()
