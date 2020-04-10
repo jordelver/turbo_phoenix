e2f0a56f805b1878a07815b605757b3cbc664966
Jordan Elver <jordan@elver.me>
2020-04-10 01:07:28 +0100

    Add Sobelow

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule TurboPhoenix.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
