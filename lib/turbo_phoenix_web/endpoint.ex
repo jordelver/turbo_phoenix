389975ab5e6ae61b8e348e8a057c395a6365a76b
Jordan Elver <jordan@elver.me>
2020-04-19 20:04:30 +0100

    Serve gzipped static assets

--- lib/turbo_phoenix_web/endpoint.ex
+++ lib/turbo_phoenix_web/endpoint.ex
@@ -21,7 +21,7 @@ defmodule TurboPhoenixWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :turbo_phoenix,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
