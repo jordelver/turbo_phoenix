282ca71e9be6c12faf76011875e1c217ffc3fa53
Jordan Elver <jordan@elver.me>
2020-04-19 16:56:18 +0100

    Remove unused CSS selectors using purgecss
    **Before**
    244kB (245kB over the wire)
    
    **After**
    42.5kB (42.6kB over the wire) - an 83% decrease in file size
    
    This is *not* removing all unused CSS, but I don't fully understand what
    heuristics are being used to decide what to remove, and I'm happy with a
    83% improvement.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,16 +1,35 @@
 const path = require('path');
-const glob = require('glob');
+const glob = require('glob-all');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+const PATHS = {
+  // Files from the Phoenix app in /lib
+  elixir: path.join(__dirname, '../lib'),
+
+  // JavaScript code under /assets/js/
+  js: path.join(__dirname, 'js')
+}
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
       new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
       new OptimizeCSSAssetsPlugin({})
-    ]
+    ],
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
   },
   entry: {
     './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
@@ -36,6 +55,12 @@ module.exports = (env, options) => ({
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new PurgecssPlugin({
+      paths: glob.sync([
+        `${PATHS.elixir}/**/*`,
+        `${PATHS.js}/**/*`
+      ], { nodir: true })
+    }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
