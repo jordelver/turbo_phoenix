015fceb21c8b7964a3529f26b6a3603e62f2fe02
Jordan Elver <jordan@elver.me>
2020-04-21 22:27:27 +0100

    Extract the progress bar to a partial

--- lib/turbo_phoenix_web/views/page_view.ex
+++ lib/turbo_phoenix_web/views/page_view.ex
@@ -16,4 +16,48 @@ defmodule TurboPhoenixWeb.PageView do
   Returns the correct CSS class for page transitions
   """
   def transition(%Plug.Conn{method: "POST"} = _conn), do: ""
+
+  @doc """
+  The amount complete the progress bar should be
+  """
+  def amount_complete(%{request_path: "/signup/name"}), do: 20
+  def amount_complete(%{request_path: "/signup/address"}), do: 40
+  def amount_complete(%{request_path: "/signup/confirm"}), do: 60
+  def amount_complete(%{request_path: "/signup/thanks"}), do: 80
+
+  @doc """
+  The textual percentage amount complete
+  """
+  def percentage_complete(%{request_path: "/signup/name"}), do: "25%"
+  def percentage_complete(%{request_path: "/signup/address"}), do: "50%"
+  def percentage_complete(%{request_path: "/signup/confirm"}), do: "75%"
+  def percentage_complete(%{request_path: "/signup/thanks"}), do: "100%"
+
+  @doc """
+  Outputs the CSS class "complete" if the step is complete
+  """
+  def step_complete?(step, %Plug.Conn{request_path: path}), do: complete?(step, path)
+
+  # name step
+  def step_complete?(:name, "/signup/name"), do: "complete"
+  def step_complete?(:name, "/signup/address"), do: "complete"
+  def step_complete?(:name, "/signup/address"), do: "complete"
+  def step_complete?(:name, "/signup/confirm"), do: "complete"
+  def step_complete?(:name, "/signup/thanks"), do: "complete"
+
+  # address step
+  def step_complete?(:address, "/signup/address"), do: "complete"
+  def step_complete?(:address, "/signup/address"), do: "complete"
+  def step_complete?(:address, "/signup/confirm"), do: "complete"
+  def step_complete?(:address, "/signup/thanks"), do: "complete"
+
+  # confirm step
+  def step_complete?(:confirm, "/signup/confirm"), do: "complete"
+  def step_complete?(:confirm, "/signup/thanks"), do: "complete"
+
+  # thanks step
+  def step_complete?(:thanks, "/signup/thanks"), do: "complete"
+
+  # step not complete
+  def step_complete?(_step, _path), do: nil
 end
