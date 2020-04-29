2045a5cb2606ee97ee676ce9dab04bdf4eb2399b
Jordan Elver <jordan@elver.me>
2020-04-29 15:50:46 +0100

    Upgrade LiveDashboard to 0.2.1 and enable `:os_mon`
    > The OS Data comes from the os_mon application, which ships as part of
    > your Erlang distribution

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule TurboPhoenix.MixProject do
   def application do
     [
       mod: {TurboPhoenix.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
